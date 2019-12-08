916a6da45ee7377d3b5f5ae2139f66bb01553fb4
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2019-12-08 10:46:12 +0100

    Updates

---
+++ precache-manifest.ec0853290567de1d2787bc23a2578aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33de021f007bdc8956a90af73c19efbd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d788a078e42145cfe4d9",
+    "url": "/monsters-rolodex/static/css/main.e6b514f3.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d788a078e42145cfe4d9",
+    "url": "/monsters-rolodex/static/js/main.fa00289b.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
